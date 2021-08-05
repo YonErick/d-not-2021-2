5aab6935a2b490fdf38050c0b60fbe323dcddc46
YonErick <weslleyfyrst@gmail.com>
2021-08-05 01:37:36 +0000

    Menu do neto socorro

---
+++ menu/src/componentes/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Menu from './menuhorizontal.js';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Ola mundo!</h1>
+    </div>
+  );
+}
+
+export default App;
