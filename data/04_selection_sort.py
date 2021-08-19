2fc789c81c78df935a95cbe081dfa92b50fd1661
YonErick <weslleyfyrst@gmail.com>
2021-08-19 23:29:48 +0000

    Bubble sort

---
+++ data/04_selection_sort.py
@@ -0,0 +1,9 @@
+# Algoritimo de ordenação selection sort
+#
+# Isola (seleciona) o primeiro elemento da lista e, em seguida,
+# encontra o menor valor restante da lista. Se o valor encontrado
+# for menor que o selecionado, efetua troca.
+# Em seguida, isola o segundo número da lista, buscando pelo menor
+# valor das posições subsequentes. Faz a troca entre dois valores,
+# se necessário. Continua o processo, até isolar o penultimo elemento 
+# da lista
