1f32094619d16ff34b5cec4e675c0113867c7b62
YonErick <weslleyfyrst@gmail.com>
2021-08-26 23:36:29 +0000

    aula05

---
+++ data/05_recursividade.py
@@ -0,0 +1,50 @@
+# Fatorial é um numero n
+# É igual ao número n multiplicado por todos os seus
+# antecessores até 1
+#
+# 7! = 7 * 6 * 5 * 4 * 3 * 2 * 1 (5840)
+# 
+# 5! = 5 * 4 * 3 * 2 * 1 (120)
+# 4! = 4 * 3 * 2 * 1 (24)
+# 3! = 3 * 2 * 1 (6)
+# 2! = 2 * 1 (2)
+# 1! = 1
+# 0! = 1 (por convenção)
+# 
+# 5! = 5 * 4!
+# 4! = 4 * 3!
+# 3! = 3 * 2!
+# 2! = 2 * 1!
+# 1! = 1 * 0!
+#
+# n! = n * (n - 1)! p/ n > 1
+
+def fatorial(n):
+    resultado = 1
+    if(n > 1):
+        # range começa no número n e vai até 1
+        for i in range(n, 1, -1):
+            resultado *= i
+            print(f'Resultado: {resultado}, i: {i}')
+    return resultado
+
+print(f'5! = {fatorial(5)}')
+print(f'7! = {fatorial(7)}')
+
+# n! = n * (n - 1)! p/ n > 1
+# Recursividade ocorre quando a definição de uma função inclui a própria 
+# função sendo definida
+
+# Em programação, a recursividade se traduz quando uma função efetua 
+# chamadas a si própria
+
+def fatorial2(n):
+    if n <= 1:
+        return 1
+    return n * fatorial2(n - 1)
+
+print('-----------------------------------------------------------')
+print(f'5! = {fatorial(5)}')
+
+# import sys
+#print(sys.maxsize)
