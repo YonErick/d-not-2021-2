8c0c8a9d9c6a02aa39d1ebd01404d53c39911264
YonErick <weslleyfyrst@gmail.com>
2021-09-16 00:46:54 +0000

    Menu Neto

---
+++ menu/src/componentes/Missao/Missao.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import './Missao.css'
+import Titulo from '../Titulo/Titulo.jsx'
+
+export default function Missao() {
+    return (
+        <div>
+
+            <Titulo texto = "Seção de Missão da Empresa" />
+
+            <div className="missao">
+
+            </div>
+
+        </div>
+
+    )
+}
+
